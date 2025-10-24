72b84043744c23184c1bc1803ab362254f24c23c
lufas2207 <yogriss111@gmail.com>
2025-10-24 16:28:35 +0700

    menghapus

--- codelab modul2 sms2/src/Main.java
+++ codelab modul2 sms2/src/Main.java
@@ -14,6 +14,7 @@ class Book {
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -21,6 +22,7 @@ class Book {
     public String getAuthor() {
         return author;
     }
+
     public void setAuthor(String author) {
         this.author = author;
     }
@@ -28,6 +30,7 @@ class Book {
     public int getStock() {
         return stock;
     }
+
     public void setStock(int stock) {
         this.stock = stock;
     }
@@ -35,6 +38,7 @@ class Book {
     public double getPrice() {
         return price;
     }
+
     public void setPrice(double price) {
         this.price = price;
     }
@@ -44,46 +48,4 @@ class Book {
         return price - (price * DISCOUNT_RATE);
     }
 
-    public void displayInfo() {
-        System.out.println("Title: " + title);
-        System.out.println("Author: " + author);
-        System.out.println("Stock: " + stock);
-        System.out.println("Price after discount: " + calculateDiscount());
-    }
-}
-
-class Library {
-    // Encapsulate Field
-    private Book book;
-    private String location;
-
-    public void setBook(Book book) {
-        this.book = book;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public void displayLibrary() {
-        System.out.println("Library location: " + location);
-        book.displayInfo();
-    }
-}
-
-// Move Method: main dipindahkan ke class Main
-public class Main {
-    public static void main(String[] args) {
-        Book book = new Book();
-        book.setTitle("Clean Code");
-        book.setAuthor("Robert C. Martin");
-        book.setStock(5);
-        book.setPrice(150000);
-
-        Library library = new Library();
-        library.setBook(book);
-        library.setLocation("Malang");
-
-        library.displayLibrary();
-    }
 }
